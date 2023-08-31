40e465c696c9014ea71cfcdeb9526723c21e5e60
Guilhermexande <guilherme.xande25@gmail.com>
2023-08-31 16:56:39 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import '../src/css/style.css'
 
 const Home = React.lazy(() => import('./views/Home/Home'))
 const Languages = React.lazy(() => import('./views/Languages/Languages'))
@@ -12,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
     <Routes>
-    <Route path="/home" name="home"element={<Home></Home>}> </Route>
+    <Route path="*" name="home"element={<Home></Home>}> </Route>
     <Route path="/languages" name="languages"element={<Languages></Languages>}> </Route>
     <Route path="/programming" name="programming"element={<Programming></Programming>}> </Route>
     <Route path="/projecthistory" name="projecthistory"element={<ProjectHistory></ProjectHistory>}> </Route>
